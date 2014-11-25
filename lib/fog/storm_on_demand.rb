d464509f91eb4c9d5d9b1456a6530a206edbded7
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2014-11-24 22:18:00 -0200

    Migrated code from Main fog gem

--- lib/fog/storm_on_demand.rb
+++ lib/fog/storm_on_demand.rb
@@ -1,4 +1,13 @@
-require "fog/storm_on_demand/version"
+require 'fog/storm_on_demand/version'
+require 'fog/storm_on_demand/account'
+require 'fog/storm_on_demand/billing'
+require 'fog/storm_on_demand/compute'
+require 'fog/storm_on_demand/dns'
+require 'fog/storm_on_demand/monitoring'
+require 'fog/storm_on_demand/network'
+require 'fog/storm_on_demand/storage'
+require 'fog/storm_on_demand/support'
+require 'fog/storm_on_demand/vpn'
 
 module Fog
   module StormOnDemand
